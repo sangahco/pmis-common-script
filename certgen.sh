78b914393b4fb1934a5e098e8155f3700fb444a0
Emanuele Disco <emawind84@gmail.com>
2018-01-31 15:32:17 +0900

    update

--- certgen.sh
+++ certgen.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
-# this script have to be used together with certbot script docker17-auto.sh
-# you don't need this if you are using normal docker-auto.sh
+# this script have to be used together with certbot service (https://github.com/sangahco/nginx-certbot)
+# AUTOSCRIPT can be changed to docker17-auto.sh if using old Centos 6.x
+
+AUTOSCRIPT=docker-auto.sh
 
 export CERTBOT_CERTS_PATH=/etc/letsencrypt
 export CERTBOT_WEBROOT=$1
 export CERTBOT_HOST=$2
 
-/root/nginx-certbot/docker17-auto.sh up
+/root/nginx-certbot/$AUTOSCRIPT up
