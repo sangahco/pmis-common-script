512e297269854364adfd0d457ab331bbfde58e43
Emanuele Disco <emawind84@gmail.com>
2022-03-11 12:35:31 +0900

    - added prepare-pmis - fixed fix_doc.sh

--- fix_doc.sh
+++ fix_doc.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env sh
 
-DOMAIN=lgsp.kspmis.com
+DOMAIN=pmis.snicm.co.kr
+NEW_DOMAIN=pmis.snicm.co.kr
 
+# prefix /ext/style/COMMON/image/icon with domain
 find . -type f -name "*.html" -exec sed -i "s/url(\/ext\/style\/COMMON\/image\/icon\//url(\/\/$DOMAIN\/ext\/style\/COMMON\/image\/icon\//g" {} \;
+# replace protocol http:// -> //
 find . -type f -name "*.html" -exec sed -i "s/http:\/\/$DOMAIN/\/\/$DOMAIN/g" {} \;
+# replace protocol https:// -> //
 find . -type f -name "*.html" -exec sed -i "s/https:\/\/$DOMAIN/\/\/$DOMAIN/g" {} \;
-
+# replace domain with new one 
+find . -type f -name "*.html" -exec sed -i "s/$DOMAIN/$NEW_DOMAIN/g" {} \;
+# replace about: -> https:
 find . -type f -name "*.html" -exec sed -i "s/about:/https:/g" {} \;
---
+++ prepare-pmis.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git clone https://github.com/sangahco/docker-webapp-hub.git hub
+git clone https://github.com/sangahco/docker-pmis-app.git pmis
+git clone https://github.com/sangahco/docker-log-shippers.git log-shipper
+git clone https://github.com/sangahco/docker-sensu.git sensu-client
